999ac943f09a6063c13861f29fcec4b7e06ecdf2
Chung Nguyen <zrmedia9@gmail.com>
2024-05-20 21:16:26 +0700

    chore: Update npm dependencies and remove unused code

--- app/components/Profile/Hero.tsx
+++ app/components/Profile/Hero.tsx
@@ -11,23 +11,37 @@ function Hero() {
       {/* Grid */}
       <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
         <div className="lg:col-span-3">
-          <h1 className={`block text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl gradient-text ${wrapperClassName}`}>{typedText}</h1>
+          <h1
+            className={`block text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl gradient-text ${wrapperClassName}`}
+          >
+            {typedText}
+          </h1>
           <div className="bio-description md:text-lg leading-10 text-textSmColor">
             <div className="my-10 text-lg xl:text-xl font-light">
               <p className="my-4">Welcome to my personal website!</p>
-              <p className="mb-5 xs:mb-10 font-light">I am a fullstack developer passionate about delivering high-quality products. I enjoy building software that improves lives by writing elegant, performant frontend code and robust, secure backend solutions. When I&apos;m not coding, I love enjoying scenic views and taking long walks.</p>
+              <p className="mb-5 xs:mb-10 font-light">
+                I am a fullstack developer passionate about delivering
+                high-quality products. I enjoy building software that improves
+                lives by writing elegant, performant frontend code and robust,
+                secure backend solutions. When I&apos;m not coding, I love
+                enjoying scenic views and taking long walks.
+              </p>
             </div>
           </div>
         </div>
         {/* End Col */}
         <div className="lg:col-span-4 mt-10 lg:mt-0">
-          <img className="w-full rounded-xl" src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=700&q=80" alt="" />
+          <img
+            className="w-full rounded-xl"
+            src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=700&q=80"
+            alt=""
+          />
         </div>
         {/* End Col */}
       </div>
       {/* End Grid */}
-    </div >
-  )
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
