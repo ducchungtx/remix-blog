d8d36e9c0a781454016f8c3f737b2020d5cb2c83
Chung Nguyen <zrmedia9@gmail.com>
2024-05-21 21:49:08 +0700

    chore: Update vitest.config.ts to use @remix-run/dev plugin
    This commit updates the vitest.config.ts file to use the @remix-run/dev plugin instead of the react plugin. It also adds the installGlobals function from @remix-run/node. These changes are made to improve the development environment for Remix Run.
    
    Co-authored-by: [Author Name]

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,17 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
+import { vitePlugin as remix } from "@remix-run/dev";
+// import react from "@vitejs/plugin-react";
 import { remixDevTools } from "remix-development-tools";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vitest/config";
 
+installGlobals();
+
 export default defineConfig({
-  plugins: [remixDevTools(), react(), tsconfigPaths()],
+  plugins: [remixDevTools(), remix(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "happy-dom",
